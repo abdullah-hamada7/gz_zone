6b0aa0a2314c24364a2005b6900f42ee7bd49730
abdullahhamada7 <chief.abdullah14@gmail.com>
2026-07-25 22:58:20 +0300

    fix: update Tripadvisor profile URL for accuracy in platform ratings

--- src/data.ts
+++ src/data.ts
@@ -507,8 +507,7 @@ export const FAQS: FAQ[] = [
 
 export const PLATFORM_RATINGS: PlatformRating[] = [
   { platform: "Google", rating: 5.0, review_count: 41, profile_url: "https://g.page/r/CeTmnPuZR9q3EBM/review" },
-  { platform: "Tripadvisor", rating: 5.0, review_count: 50, profile_url: "https://www.tripadvisor.com/UserReviewEdit-g189180-d34355172-Gz_zone_Massage_Cupping_Therpay_Porto-Porto_Porto_District_Northern_Portugal.html" },
-  { platform: "Wanderlog", rating: 5.0, review_count: 129, profile_url: "https://wanderlog.com/place/details/12672638/gzzone-massage--cupping-therapy-istanbul" },
+  { platform: "Tripadvisor", rating: 5.0, review_count: 50, profile_url: "https://www.tripadvisor.com/Attraction_Review-g189180-d34355172-Reviews-Gz_zone_Massage_Cupping_Therpay_Porto-Porto_Porto_District_Northern_Portugal.html" },
   { platform: "WhatClinic", rating: 5.0, review_count: 15, profile_url: "https://www.whatclinic.com/consumer/reviewslead.aspx?clinicid=302529" },
 ];
 
